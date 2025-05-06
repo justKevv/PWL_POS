e47e2196376db88111cf17337e25833d6fefe116
justKevv <kevinbramastaa@gmail.com>
2025-05-06 07:13:51 +0700

    feat(auth): add login endpoint with authentication logic

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 Route::post('/register', RegisterController::class)->name('register');
+Route::post('/login', LoginController::class)->name('login');
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
